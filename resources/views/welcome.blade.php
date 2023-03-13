7ef4cbbfc37e16791c3c1070d4661cb2df4cc1e1
Zineb Atlassi <atlassizineb@gmail.com>
2023-03-13 17:50:22 +0100

    Inizializzazione file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 + Bootstrap Template</title>
+        <title>Laravel Model Controller</title>
 
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
     </head>
     <body>
 
-        <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
-
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </main>
+        
 
     </body>
 </html>
